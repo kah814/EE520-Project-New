20ee19157010209321e56b5ccb1359f56efd635c
kennethhurt1991 <kennethhurt1991@gmail.com>
2021-06-14 20:33:33 -0700

    pushing leader.cc file

---
+++ my_project/src/leader.cc
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <math.h>
+#include "leader.h"
+
+using namespace enviro;
+
+// Note : All implementation is in Leader but the Makefile
+//        needs a .cc file to know what .so files to make
